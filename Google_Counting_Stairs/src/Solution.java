7ac089e728d9b3ad48ece680d62ec774ca8074fa
HsunFu Liu <lucky20511@gmail.com>
2017-02-21 09:40:20 -0800

    Modify the third method on Counting Stairs

--- Google_Counting_Stairs/src/Solution.java
+++ Google_Counting_Stairs/src/Solution.java
@@ -3,12 +3,29 @@ public class Solution {
 
 	public static void main(String[] args){
 		Solution sol = new Solution();
-		System.out.println(20+" "+sol.countStairs(200));
-		
-		
-		
+		System.out.println(200+" "+sol.countStairs(200));
+
 	}
 	
+	
+	
+	/*
+	 * 
+	 * dp[i,j] = SUM( dp[i-k,j-i-k] )  i-k > 0,  j-i-k > 0 
+	 * 
+	 */
+	
+	public int countStairs(int num){
+		int[][] dp = new int[num+1][num+1];
+		
+		for(int i = 1; i <= num; i++){
+			dp[i][0] = 1;
+			for(int j = 1; j <= num; j++)
+				for(int k = 1; k < i && j-k >= 0; k++)
+					dp[i][j] += dp[k][j-k];
+		}
+		return dp[num][num];
+	}
 	
 	
 	/*
@@ -16,24 +33,7 @@ public class Solution {
 	 * dp[i,j] =dp[i-j,j] + dp[i-j,j+1] 
 	 * 
 	 */
-	
-//	public int countStairs(int num){
-//
-//		int[][] dp = new int[num+1][num+1];
-//		for(int i = 2; i <= num; i++){
-//			dp[i][i] = 1;
-//			for(int j = 1; j < i; j++){
-//				if(i == num && j == 2){
-//					break;
-//				}
-//				dp[i][j] = dp[i-j][j] + dp[i-j][j+1];
-//			}
-//		}
-//		return dp[num][1];
-//	}
-	
-	
-	
+
 //	public int countStairs(int num){
 //		int[][] dp = new int[num+1][num+2];
 //		for(int i = 2; i <= num; i++) dp[i][i] = 1;
@@ -44,13 +44,19 @@ public class Solution {
 	
 	
 	
-	public int countStairs(int num){
-		int[] dp = new int[num+1];
-		dp[0] = 1;
-		for(int i = 1; i <= num; i++)
-			for(int j = num; j >= i; j--)
-				dp[j] += dp[j-i];
-		return dp[num]-1;
+	 /*
+	  * 
+	  * dp[i] = dp[i-j]
+	  * 
+	  */
 
-	}
+//	public int countStairs(int num){
+//		int[] dp = new int[num+1];
+//		dp[0] = 1;
+//		for(int i = 1; i <= num; i++)
+//			for(int j = num; j >= i; j--)
+//				dp[j] += dp[j-i];
+//		return dp[num]-1;
+//
+//	}
 }
