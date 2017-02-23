6b1f8cc6d88d4c79bad79d89bc7fa6d81b316c79
HsunFu Liu <lucky20511@gmail.com>
2017-02-22 18:30:07 -0800

    Add Google Two Bomb Problem

---
+++ Google_Two_Bomb/src/Solution.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Solution {
+	
+	public static void main(String[] args){
+		Solution sol = new Solution();
+		System.out.println(sol.TwoBomb(6, 7));
+	}
+	
+	public int TwoBomb(int i, int j){
+		
+		int count = 0;
+		while(i != 1 || j != 1){
+			if(j > i) j -= i;
+			else i -= j;
+			count++;
+		}
+		return count;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	public int TwoBomb(int i, int j){
+//		
+//		int count = 0;
+//		while(i != 1 || j != 1){
+//			if(j > i){
+//				j -= i;
+//			}else{ // j <= i
+//				i -= j;
+//			}
+//			count++;
+//			
+//		}
+//		return count;
+//	}
+}
