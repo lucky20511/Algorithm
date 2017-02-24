335ab5332a1a3a389620b2b3f31518b8d0171c2b
HsunFu Liu <lucky20511@gmail.com>
2017-02-23 19:15:11 -0800

    Add Contiguous Array Problem

--- 525_Contiguous_Array/src/Solution.java
+++ 525_Contiguous_Array/src/Solution.java
@@ -2,4 +2,34 @@ import java.util.*;
 
 public class Solution {
 
+	public static void main(String[] args){
+		Solution sol = new Solution();
+		System.out.println(sol.findMaxLength(new int[]{0,1,1,0,1,1,1,0}));
+		
+		
+	}
+	
+	
+	public int findMaxLength(int[] nums) {
+        
+        int MaxLen = 0;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        map.put(0,-1);  // very important
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0){
+                nums[i] = -1;
+            }
+            sum += nums[i];
+            if(map.containsKey(sum)){
+            	MaxLen = Math.max(MaxLen, i-map.get(sum));
+            }else{
+                map.put(sum, i);
+            }
+        }
+        return MaxLen;
+    }
+	
+	
+	
 }
