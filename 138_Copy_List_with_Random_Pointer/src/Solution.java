a32e0ec60dc9e20189f1afc889cd2e53578f314b
HsunFu Liu <lucky20511@gmail.com>
2017-03-03 22:36:51 -0800

    Add Copy List with Random Pointer Problem

---
+++ 138_Copy_List_with_Random_Pointer/src/Solution.java
@@ -0,0 +1,33 @@
+
+public class Solution {
+
+	/*
+	 *  Method I: recursion + HashMap
+	 */
+	
+	public RandomListNode copyRandomList(RandomListNode head) {
+		Map<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
+		map.put(null, null);
+		return helper(map, head);
+	}
+
+	private RandomListNode helper(Map<RandomListNode, RandomListNode> map, RandomListNode node){
+		if(map.containsKey(node)){
+			return map.get(node);
+		}
+		RandomListNode new_node = new RandomListNode(node.label);
+		map.put(node, new_node);
+		new_node.next = helper(map, node.next);
+		new_node.random = helper(map, node.random);
+		//map.put(node, new_node);
+		return new_node;
+
+		
+	}
+	
+	
+	/*
+	 *  Method II: Iteration + HashMap
+	 */
+
+}
