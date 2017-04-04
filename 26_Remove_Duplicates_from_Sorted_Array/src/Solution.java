630e7fc76f6ff6f189e4f0bcc0f9c889e39b4348
HsunFu Liu <lucky20511@gmail.com>
2017-04-03 20:07:17 -0700

    Add Remove Duplicates From Sorted Array Problem

---
+++ 26_Remove_Duplicates_from_Sorted_Array/src/Solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args){
+		
+	}
+	
+	public int removeDuplicates(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+        int p1 = 0;
+        for(int p2 = 1; p2 < nums.length; p2++){
+            if(nums[p2] != nums[p1]){
+                nums[++p1] = nums[p2];
+            }
+        }
+        return p1+1;
+    }
+	
+}
